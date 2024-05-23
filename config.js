d0c668f8e8243a43c7661267f6b848e309c52f5e
sachind3 <desai.sachin45@gmail.com>
2024-05-23 22:31:38 +0530

    wip

--- config.js
+++ config.js
@@ -3,10 +3,7 @@ mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
