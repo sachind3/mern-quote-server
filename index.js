b235dad341d590f65d9393442effeb1bebd00fde
sachind3 <desai.sachin45@gmail.com>
2024-05-23 21:15:03 +0530

    wip

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const connectDB = require("./config");
 const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(
   cors({
